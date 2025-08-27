b87b1259019d7736799fcc1a7013c53a89e41bdb
Sdg877 <sdrakegill@gmail.com>
2025-08-27 06:21:52 +0100

    Footprint router added.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routes import users
+from .routes import users, footprints
 
 app = FastAPI()
 
-# Create tables
 models.Base.metadata.create_all(bind=engine)
 
-# Add user routes
 app.include_router(users.router)
+app.include_router(footprints.router)
 
 @app.get("/")
 def root():
