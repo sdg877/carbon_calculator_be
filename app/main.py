18bae6d0a476eb0ddfc333464ecb198b671fe4a6
Sdg877 <sdrakegill@gmail.com>
2025-09-01 06:12:01 +0100

    Offset suggestion endpoint added.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routes import users, footprints
+from .routes import users, footprints, suggestions
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(users.router)
-app.include_router(footprints.router)
+app.include_router(users.router, prefix="/users", tags=["users"])
+app.include_router(footprints.router, prefix="/footprints", tags=["footprints"])
+app.include_router(suggestions.router, prefix="/offsets", tags=["offsets"])
 
 @app.get("/")
 def root():
