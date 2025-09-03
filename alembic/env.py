fd1841afef618bcfa498e4c38b35943ddd2ee771
Sdg877 <sdrakegill@gmail.com>
2025-09-03 05:55:29 +0100

    Update to env file.

--- alembic/env.py
+++ alembic/env.py
@@ -3,16 +3,16 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 import sys
 import os
+from app.database import Base
+from app import models  
 
-# Add app folder to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
 
-from app.models import Base  # Import your SQLAlchemy Base
+from app.models import Base 
 
 config = context.config
 fileConfig(config.config_file_name)
 
-# This is required for autogenerate
 target_metadata = Base.metadata
 
 def run_migrations_offline():
