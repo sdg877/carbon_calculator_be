3d1c888f1982da7ec710cd9aac4f9ccad2914e62
Sdg877 <sdrakegill@gmail.com>
2025-11-26 06:23:01 +0000

    Update user schema added.

--- app/schemas.py
+++ app/schemas.py
@@ -2,27 +2,40 @@ from typing import Optional, List, Dict
 from pydantic import BaseModel
 from datetime import datetime
 
+
 class UserBase(BaseModel):
     username: str
+
 
 class UserCreate(UserBase):
     password: str
 
+
 class UserResponse(UserBase):
     id: int
+
     class Config:
         from_attributes = True
+
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+
 
 class Token(BaseModel):
     access_token: str
     token_type: str
-    
+
+
 class FootprintBase(BaseModel):
     activity_type: str
     details: Optional[Dict] = None
 
+
 class FootprintCreate(FootprintBase):
     pass
+
 
 class FootprintResponse(FootprintBase):
     id: int
@@ -35,26 +48,9 @@ class FootprintResponse(FootprintBase):
         from_attributes = True
 
 
-# class FootprintBase(BaseModel):
-#     activity_type: str
-#     carbon_kg: float
-#     details: Optional[Dict] = None
-
-# class FootprintCreate(FootprintBase):
-#     pass
-
-# class FootprintResponse(FootprintBase):
-#     id: int
-#     user_id: int
-#     created_at: datetime
-#     suggested_offsets: Optional[List[str]] = []
-
-#     class Config:
-#         from_attributes = True
-
 class FootprintAverageResponse(BaseModel):
     created_at: datetime
     carbon_kg: float
-    
+
     class Config:
         from_attributes = True
