40b050d612e748ae71fff5fda88e647a37f4b9a5
Sdg877 <sdrakegill@gmail.com>
2026-01-27 15:30:22 +0000

    Update to import statement.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .database import engine
-from app.routes import users, footprints, suggestions
+from .routes import users, footprints, suggestions
 
 app = FastAPI()
 
@@ -12,7 +12,6 @@ app.include_router(users.router, tags=["users"])
 app.include_router(footprints.router, tags=["footprints"])
 app.include_router(suggestions.router, prefix="/offsets", tags=["offsets"])
 
-# CORS middleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
