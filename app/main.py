e1725dc54faad3b9e9424c611f6c4e304f41d818
Sdg877 <sdrakegill@gmail.com>
2026-02-12 13:45:33 +0000

    Update to cors policy.

--- app/main.py
+++ app/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "https://carbon-calculator-fe-pi.vercel.app",
+        "http://localhost:3000"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
