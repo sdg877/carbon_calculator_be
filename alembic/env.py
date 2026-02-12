fed68e86cf549b38306cf049165227ebe5e3bb1f
Sdg877 <sdrakegill@gmail.com>
2026-02-12 13:40:48 +0000

    Update to function to connect to database.

--- alembic/env.py
+++ alembic/env.py
@@ -27,18 +27,18 @@ def run_migrations_offline():
         context.run_migrations()
 
 def run_migrations_online():
+    from dotenv import load_dotenv
+    load_dotenv()
+    config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+
     connectable = engine_from_config(
         config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
-        poolclass=pool.NullPool
+        poolclass=pool.NullPool,
     )
 
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
+
         with context.begin_transaction():
             context.run_migrations()
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
